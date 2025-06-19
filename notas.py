c9fe8c9a6f3d565d1824e4402f132809321cad38
Igor Gaudencio <igorgaudencio@live.com>
2025-06-19 09:13:40 -0300

    Calculadora simples de media de notas

---
+++ notas.py
@@ -0,0 +1,21 @@
+def media_notas():
+    alunos = 0
+    soma = 0
+    media = 0
+
+    nota1 = float ( input ( "Digite a primeira nota:") )
+    nota2 = float ( input ( "Digite a segunda nota:") )
+    nota3 = float ( input ( "Digite a terceira nota:") )
+
+    soma = nota1 + nota2 + nota3
+    media = soma/3
+
+    print ( "nota = ", media )
+    if media >= 7:
+        print ("Aluno aprovado")
+    
+    else:
+        print ("Aluno reprovado")
+    return 0
+
+media_notas()
